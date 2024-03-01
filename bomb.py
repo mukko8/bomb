557a9fb43914876431a2cf23cc166669645e0e6d
steel <gawct.231020@gmail.com>
2024-03-01 14:00:04 +0900

    カウントダウン修正

--- bomb.py
+++ bomb.py
@@ -7,7 +7,7 @@ cvs=tk.Canvas(width=900,height=600)
 cvs.pack()
 #bg=tk.PhotoImage(file='bg.png')
 bg='white'
-cvs.create_image(450,300,)#image=bg)
+cvs.create_image(450,200,)#image=bg)
 msg=tk.Label(
         text='○○ゲーム',
         bg='yellow',
@@ -19,7 +19,7 @@ class Timer:
     def __init__(self,master):
         self.master=master
         self.master.title("残り時間")
-        self.remaining_time=10
+        self.remaining_time=10.00
 
         self.label=tk.Label(master,font=("Helvetica",24))
         self.label.pack(pady=20)
@@ -34,12 +34,12 @@ class Timer:
 
     def update_timer(self):
         if self.remaining_time>0:
-            self.label.config(text=f'残り{self.remaining_time}秒')
-            self.remaining_time -= 1
-            self.master.after(1000,self.update_timer)
+            self.label.config(text=f'残り{self.remaining_time:.2f}秒')
+            self.remaining_time -= 0.01
+            self.master.after(10,self.update_timer)
 
         else:
             self.start_button.config(state=tk.DISABLED)
-app=Timer(root)
 
+app=Timer(root)
 root.mainloop()
