5ab8f94e88901fbfad6c502d11918e12db411e4c
steel <gawct.231020@gmail.com>
2024-03-01 12:48:44 +0900

    canvas作成

--- bomb.py
+++ bomb.py
@@ -1,1 +1,16 @@
 import tkinter as tk
+import random
+
+root=tk.Tk()
+cvs=tk.Canvas(width=900,height=600)
+cvs.pack()
+#bg=tk.PhotoImage(file='bg.png')
+bg='white'
+cvs.create_image(450,300,)#image=bg)
+msg=tk.Label(
+        text='○○ゲーム',
+        bg='yellow',
+        fg='black'
+        )
+msg.place(x=200,y=30)
+root.mainloop()
