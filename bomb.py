8197ed93fa4079d6fea453723e14a0c451dcf4a1
steel <gawct.231020@gmail.com>
2024-03-01 14:41:29 +0900

    修正途中

--- bomb.py
+++ bomb.py
@@ -2,18 +2,6 @@ import tkinter as tk
 import random
 from tkinter import messagebox
 
-root=tk.Tk()
-cvs=tk.Canvas(width=900,height=600)
-cvs.pack()
-#bg=tk.PhotoImage(file='bg.png')
-bg='white'
-cvs.create_image(450,200,)#image=bg)
-msg=tk.Label(
-        text='○○ゲーム',
-        bg='yellow',
-        fg='black'
-        )
-msg.place(x=200,y=30)
 
 class Timer:
     def __init__(self,master):
@@ -23,6 +11,10 @@ class Timer:
 
         self.label=tk.Label(master,font=("Helvetica",24))
         self.label.pack(pady=20)
+
+        button_frame=tk.Frame(master)
+        button_frame.pack()
+
         self.start_button=tk.Button(master,text="Start",command=self.start_timer)
         self.start_button.pack()
 
@@ -40,6 +32,20 @@ class Timer:
 
         else:
             self.start_button.config(state=tk.DISABLED)
+
+root=tk.Tk()
+root.geometry("900x600")
+cvs=tk.Canvas(width=900,height=600)
+cvs.pack()
+#bg=tk.PhotoImage(file='bg.png')
+bg='white'
+cvs.create_image(450,300)#,image=bg)
+msg=tk.Label(
+        text='○○ゲーム',
+        bg='yellow',
+        fg='black'
+        )
+msg.place(x=200,y=30)
 
 app=Timer(root)
 root.mainloop()
