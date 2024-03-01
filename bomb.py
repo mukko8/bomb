a087220cb43f87045c42a0af7d4a6d7686f5a527
steel <gawct.231020@gmail.com>
2024-03-01 13:37:42 +0900

    カウントダウン追加

--- bomb.py
+++ bomb.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random
+from tkinter import messagebox
 
 root=tk.Tk()
 cvs=tk.Canvas(width=900,height=600)
@@ -13,4 +14,32 @@ msg=tk.Label(
         fg='black'
         )
 msg.place(x=200,y=30)
+
+class Timer:
+    def __init__(self,master):
+        self.master=master
+        self.master.title("残り時間")
+        self.remaining_time=10
+
+        self.label=tk.Label(master,font=("Helvetica",24))
+        self.label.pack(pady=20)
+        self.start_button=tk.Button(master,text="Start",command=self.start_timer)
+        self.start_button.pack()
+
+    def start_timer(self):
+        if self.remaining_time>0:
+            self.update_timer()
+        else:
+            messagebox.showinfo("Time up!")
+
+    def update_timer(self):
+        if self.remaining_time>0:
+            self.label.config(text=f'残り{self.remaining_time}秒')
+            self.remaining_time -= 1
+            self.master.after(1000,self.update_timer)
+
+        else:
+            self.start_button.config(state=tk.DISABLED)
+app=Timer(root)
+
 root.mainloop()
