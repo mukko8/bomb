cb7b12d8378081fc1c55fa02546621e42ed5fc77
cm <taro.shimashima@gmail.com>
2024-03-04 06:45:44 +0900

    メソッド等追加

--- bomb.py
+++ bomb.py
@@ -2,7 +2,16 @@ import tkinter as tk
 import random
 
 def bt_click(e):
-    pass
+    if 'out' in str(e.widget):
+        cvs.create_image(450,300,image=images[2])
+        hide_elements()
+
+    else:
+        e.widget['image']=images[1]
+
+def hide_elements():
+    msg.place_forget()
+    msg2.place_forget()
 
 class Timer:
     def __init__(self,master):
@@ -11,33 +20,34 @@ class Timer:
         self.remaining_time=10.0
 
         self.label=tk.Label(master,font=("Helvetica",50),anchor='e',bg='#3f3f3f',fg='green')
-        self.label.place(x=405,y=280)
+        self.label.place(x=400,y=280)
 
-        button_frame=tk.Frame(master)
-        button_frame.pack()
-
-        #self.canvas=tk.Canvas(master,width=400,height=300)
-        #self.canvas.pack()
+        self.cvs=tk.Canvas(master,width=900,height=600)
+        self.cvs.pack()
 
         self.start_button=tk.Button(master,text="Start",font=("Helvetica",10),command=self.start_timer,width=15,height=2)
         self.start_button.place(x=390,y=500)
 
     def start_timer(self):
+        print("タイマーを開始しました")
         if self.remaining_time>0:
             self.update_timer()
-        elif self.remaining_time==0:
+        else:
+            print("タイマーが終了しました")
+            #img=images[2]
             img=tk.PhotoImage(file='bombed.png')
-            self.canvas.create_image(500,400,image=img)
+            self.cvs.create_image(450,300,image=img)
+            self.label.place_forget()
+            self.start_button.place_forget()
 
     def update_timer(self):
         if self.remaining_time>0:
             self.label.config(text=f'{self.remaining_time:.1f}')
             self.remaining_time -= 0.1
             self.master.after(100,self.update_timer)
-
         else:
             self.start_button.config(state=tk.DISABLED)
-
+    
 root=tk.Tk()
 root.geometry("900x600")
 cvs=tk.Canvas(width=900,height=600)
@@ -70,7 +80,7 @@ buttons=[None]*6
 for i in range(6):
     x=30+(i%2)*750
     y=100+(i//2)*150
-    b_name='safe' if lucky == i else str(i)
+    b_name='out' if lucky == i else str(i)
     buttons[i]=tk.Button(image=images[0],name=b_name)
     buttons[i].place(x=x,y=y)
     buttons[i].bind('<ButtonPress>',bt_click)
